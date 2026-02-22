efcc4c9531f91c543366fa488e92a156a9d7d9bf
Shivam Kumar <jswshivam2006@gmail.com>
2026-02-22 17:18:52 +0530

    new changes

--- app.py
+++ app.py
@@ -11,7 +11,6 @@ def home():
     return render_template("index.html")
 # @app.route("/about")
 # def about():
-@app.route("/collect",methods=["POST"])
 @app.route("/data")
 def show_data():
     try:
@@ -21,23 +20,39 @@ def show_data():
         content="Not Data Found Yet."
     return"<pre>" + content + "<pre>"
         
+@app.route("/collect",methods=["POST"])
 def collect():
     data=request.get_json(silent=True)
+
+    if not data:
+        return {"error": "No JSON received"}, 400
+    
     lat=data["lat"]
     lon=data["lon"]
+    name=data["name"]
+
+    if not lat or not lon:
+        return {"error": "Missing coordinates"}, 400
+    
     # REVERSE GEOCODING API
     url=f"https://nominatim.openstreetmap.org/reverse?format=json&lat={lat}&lon={lon}"
     headers={
         "User-Agent":"FlaskLocationApp"
     }
+    try:
+        response = requests.get(url, headers=headers)
+        address = response.json().get("display_name", "Address not found")
+    except Exception as e:
+        print("Geocoding error:", e)
+        address = "Address lookup failed"
 
-    response=requests.get(url,headers=headers)
-    address=response.json().get("display_name","Address not found")
-    print('Address :',address)
+    # response=requests.get(url,headers=headers)
+    # address=response.json().get("display_name","Address not found")
+    # print('Address :',address)
     # data["time"]=str(datetime.datetime.now().strftime("%d-%m-%Y %I:%M %p")) #02-02-2026 8:56 PM 
     data["time"]=str(datetime.datetime.now().strftime("%d %b %Y , %I:%M %p")) #02 Feb 2026 , 8:56 PM 
     with open('Location.txt',"a")as f:
-        f.write(json.dumps(data)+ "\n")
+        f.write(json.dumps(data,name)+ "\n")
         print("Saved ")
     return {
         "Status": "Location Saved",
@@ -47,5 +62,5 @@ if __name__=="__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
     # app.run(debug=True)
-    #  app.run(host="0.0.0.0", port=5000)
+    # app.run(host="0.0.0.0", port=5000)
 
