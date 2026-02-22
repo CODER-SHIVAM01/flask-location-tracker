6c7ca0e5b0d3ba219b484374027a8cfcfe950e1b
Shivam Kumar <jswshivam2006@gmail.com>
2026-02-22 13:33:40 +0530

    Final fix the code

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask import Flask,render_template,request
 import requests
 import datetime
 import json
+import os
 app=Flask(__name__)
 @app.route("/")
 def home():
@@ -32,6 +33,8 @@ def collect():
         "Status": "Location Saved",
         'address':address}
 if __name__=="__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
     # app.run(debug=True)
-     app.run(host="0.0.0.0", port=5000)
+    #  app.run(host="0.0.0.0", port=5000)
 
